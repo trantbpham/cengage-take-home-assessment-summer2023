91e628f59cde849b604e730d40a2d51b2e50726c
Chuck Thomas <chuckthemole@gmail.com>
2023-05-23 15:14:32 -0700

    Build employee class part-1 set-2 Java

--- part-1/set-2/java/challenge-1/Employee.java
+++ part-1/set-2/java/challenge-1/Employee.java
@@ -3,4 +3,52 @@
 
 public class Employee {
 
+    // member variables
+    private String name;
+    private String department;
+    private int age;
+
+    // Ctors
+    public Employee() {
+        this.name = "";
+        this.department = "";
+        this.age = 0;
+    }
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public Employee(String department, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
