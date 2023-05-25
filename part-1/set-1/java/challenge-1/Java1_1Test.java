ea72af9636c99c9edef336e58dec41f3f45b4ac8
Chuck Thomas <chuckthemole@gmail.com>
2023-05-25 14:27:28 -0700

    Build tests part-1 set-1 and set-2 Java

---
+++ part-1/set-1/java/challenge-1/Java1_1Test.java
@@ -0,0 +1,33 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Java1_1Test {
+
+    private static final String LINE_ONE = "I am learning to program in Java";
+    private static final String LINE_TWO = "That's awesome!";
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream outStrmCapture = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outStrmCapture));
+        Java1_1.main(new String[0]);
+    }
+
+    @AfterEach
+    public void restoreSystemInOut() {
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void testMatchingOutput() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LINE_ONE).append("\n").append(LINE_TWO);
+        Assertions.assertEquals(sb.toString(), outStrmCapture.toString().trim());
+    }
+}
