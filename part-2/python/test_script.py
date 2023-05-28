7c2d4c197995cd859e26c9faab73653e009990c1
Hyunjo <hyl130@ucsd.edu>
2023-05-28 15:31:41 -0700

    Test script outputs each method and its result

--- part-2/python/test_script.py
+++ part-2/python/test_script.py
@@ -1,24 +1,69 @@
 import unittest
+import io
 
-import program_1
-import program_2
-import program_3
-import program_tests
+import program_1_test
+import program_2_test
+import program_3_test
 
-def warp_test_suite(testcase_class):
-    """Load tests from a specific set of TestCase classes."""
-    suite = unittest.TestSuite()
-    tests = unittest.defaultTestLoader.loadTestsFromTestCase(testcase_class)
-    suite.addTest(tests)
-    return suite
+class Test:
 
+    # Constructor for each program
+    def __init__(self, program):
+        self.program_name = program.__name__
+        self.suite = unittest.TestLoader().loadTestsFromModule(program)
+        self.total_tests = 0
+        self.failed_tests = 0
+        self.passed_tests = 0
+        
+    # This method runs the test for the tester
+    def run_test(self):
+        out = io.StringIO()
+        print("---------------------------------------------------")
+        print("Testing:", self.program_name, "\n")
+        result_list = []
+
+        # For each of the test cases, get its result
+        for test_cases in self.suite:
+            for test in test_cases:
+                test_result = unittest.TextTestRunner(out, verbosity=2).run(test)
+                result_list.append(test._testMethodName)
+                self.total_tests += 1
+
+                # Test passed
+                if test_result.wasSuccessful():
+                    result_list.append("Passed")
+                    self.passed_tests += 1
+                # Test failed
+                else:
+                    result_list.append("Failed")
+                    self.failed_tests += 1
+        print()
+
+        # Print each of the method name and its results
+        for i in range(0, len(result_list), 2):
+            print(result_list[i])
+            print(result_list[i+1])
+            print()
+
+        # Print the test summary
+        print("-------TEST SUMMARY-------")
+        print("Total tests:", self.total_tests)
+        print("Failed:", self.failed_tests)
+        print("Passed:", self.passed_tests)
+
+        # If all the test cases passed
+        if self.failed_tests == 0:
+            print()
+            print("All tests passed")
+        print()
+        print()
+
+# main program
 if __name__ == '__main__':    
-    suite = unittest.TestLoader().loadTestsFromModule(program_tests)
-    testResult = unittest.TextTestRunner().run(suite)
+    program1 = Test(program_1_test)
+    program2 = Test(program_2_test)
+    program3 = Test(program_3_test)
 
-    if len(testResult.failures) == 0:
-        print("All tests passed")
-    else:
-        print("Total tests:", testResult.testsRun)
-        print("Failed:", len(testResult.failures))
-        print("Passed:", testResult.testsRun-len(testResult.failures))
+    program1.run_test()
+    program2.run_test()
+    program3.run_test()
