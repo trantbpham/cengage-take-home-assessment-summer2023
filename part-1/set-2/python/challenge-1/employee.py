7e336de91e3add873c6ce987bb4591805546bca2
Michael Murray <mcmurray0125@gmail.com>
2023-05-29 03:43:39 -0500

    part-1 set-2 Python Employee

--- part-1/set-2/python/challenge-1/employee.py
+++ part-1/set-2/python/challenge-1/employee.py
@@ -1,1 +1,23 @@
 class Employee:
+    def __init__(self, name="", age=0, department=""):
+        self.name = name
+        self.age = age
+        self.department = department
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+    def getDepartment(self):
+        return self.department
+
+    def setName(self, name):
+        self.name = name
+
+    def setAge(self, age):
+        self.age = age
+
+    def setDepartment(self, department):
+        self.department = department
