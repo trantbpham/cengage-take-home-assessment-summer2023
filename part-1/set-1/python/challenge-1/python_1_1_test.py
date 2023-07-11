3f959df185c8ee3015cf803f17715562f23d198c
Shane <shanekopec123@gmail.com>
2023-07-11 13:05:34 -0400

    test Change

--- part-1/set-1/python/challenge-1/python_1_1_test.py
+++ part-1/set-1/python/challenge-1/python_1_1_test.py
@@ -8,6 +8,7 @@ class Python_1_1_Tests(unittest.TestCase):
     def test_print(self):
         proc = subprocess.Popen(['python', python_1_1.__file__], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         self.assertEqual(proc.communicate()[0].decode(), "I am learning to program in Python\r\nThat's awesome!\r\n")
+        
 
 if __name__ == '__main__':
     unittest.main()
